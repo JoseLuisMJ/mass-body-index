16892e5a7e2e1a7e0e022c9eb7ab05c176977005
JoseLuisMJ <trepa01@hotmail.com>
2023-07-15 12:14:45 +0200

    add dependency hamcrest

--- src/test/java/dev/joseluis/IndexMBITest.java
+++ src/test/java/dev/joseluis/IndexMBITest.java
@@ -3,6 +3,17 @@ package dev.joseluis;
 import org.junit.jupiter.api.Test;
 
 public class IndexMBITest {
+
+    @Test
+    void testMBIHavenWeightAndHeight() {
+      IndexMBI indexMBI = new IndexMBI(87, 1.74);
+      
+      int weight = indexMBI.getWeight();
+      double height = indexMBI.getHeight();
+    
+      assertThat(weight, is(87));
+      assertThat(height, is(1.74));
+    }
     @Test
     void testCalculateMBI() {
         int weight = 87;
